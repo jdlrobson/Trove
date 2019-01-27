578afee3b8a76b1562e37c5d7014e7ebd6b5b3a5
Daniel Beard <dab1001@hotmail.co.uk>
2019-01-27 22:15:25 +0000

    Implement more in-depth skeleton for skin

--- includes/TrevelyanTemplate.php
+++ includes/TrevelyanTemplate.php
@@ -15,24 +15,52 @@ class TrevelyanTemplate extends BaseTemplate {
 <!-- START OF PAGE -->
   <?php $this->html('headelement'); ?>
     <!-- BANNER -->
-    <div if="trevelyan-banner">
-
+    <div id="trevelyan-banner">
+      <!-- BANNER SEARCH BUTTON -->
+      <div id="trevelyan-banner-search">
+        <!-- TODO: Implement search button -->
+      </div>
+      <!-- BANNER LOGO/NAV BUTTON -->
+      <div id="trevelyan-banner-navigation">
+        <!-- TODO: Implement main logo --->
+      </div>
+      <!-- BANNER MENU BUTTON -->
+      <div id="trevelyan-banner-menu">
+        <!-- TODO: Implement menu button -->
+      </div>
     </div>
-    <!-- NAVIGATIOn -->
+    <!-- NAVIGATION -->
     <div id="trevelyan-navigation">
-
+      <!-- TODO: Implement navigation fetching -->
     </div>
     <!-- MENU -->
     <div id="trevelyan-menu">
-
+      <!-- TODO: Implement menu -->
     </div>
     <!-- SEARCH -->
     <div id="trevelyan-search">
-
+      <!-- TODO: Implement search form -->
     </div>
     <!-- PAGE CONTENT -->
-    <div if="trevelyan-page">
-      
+    <div id="trevelyan-page">
+      <!-- ARTICLE HEADER -->
+      <div id="trevelyan-page-header">
+        <!-- ARTICLE TITLE -->
+        <h1 id="trevelyan-header-title">
+          <?php $this->html('title'); ?>
+        </h1>
+        <!-- EDIT BUTTON -->
+        <div id="trevelyan-header-editButton">
+          <!-- TODO: Implement edit button -->
+        </div>
+        <!-- PAGE STATUS INDICATORS -->
+        <?php echo $this->getIndicators(); ?>
+      </div>
+      <!-- TODO: Implement infobox handling -->
+      <!-- ARTICLE CONTENT --->
+      <?php $this->html('bodytext'); ?>
+      <!-- ARTIClE CATEGORIES -->
+      <?php $this->html('catlinks'); ?>
     </div>
   <?php $this->printTrail(); ?>
 <!-- END OF PAGE -->
